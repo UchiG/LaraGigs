300279a1dac08a6b6c4375dff641937f6c801a5b
Uchirai <uchirai.govere@gmail.com>
2023-05-29 12:32:27 +0200

    testing

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,16 @@
-<h1>{{$heading}}</h1>
-@foreach($listings as $listing)
-<h2>{{$listing['title']}}</h2>
-<p>{{$listing['description']}}</p>
+<h1>{{ $heading }}</h1>
+@php
+$test = 'test';
+@endphp
+{{$test}}
 
-<?php endforeach; ?>
+@foreach ($listings as $listing)
+    <h2>
+        {{ $listing['title'] }}
+    </h2>
+    <p>
+        {{ $listing['description'] }}
+    </p>
+
+@endforeach
+    
