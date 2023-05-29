a10fcb563f4e77d8c3b5e2008a047e5b5a63ee54
Uchirai <uchirai.govere@gmail.com>
2023-05-29 23:24:58 +0200

    added data.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing {
-    public static function all() {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'House in the woods'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'House in the city'
-            ],
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
